cd4c4bf6316b195cff4ae7411739ebec5257d502
Svante Nilsson <svante.n@live.se>
2015-04-07 22:07:46 +0200

    Some small changes in the report draft

---
+++ compile_latex.bat
@@ -0,0 +1,7 @@
+@ECHO off
+
+rm "utkast till utkast till rapport.log"
+rm "utkast till utkast till rapport.aux"
+rm "utkast till utkast till rapport.pdf"
+
+pdflatex "utkast till utkast till rapport.tex"
