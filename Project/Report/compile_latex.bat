b84dff87c0cc34a2cdfcb719dfa525c43b0ab563
Svante Nilsson <svante.n@live.se>
2015-04-26 15:16:05 +0200

    compile_latex.bat now compiles references

--- Project/Report/compile_latex.bat
+++ Project/Report/compile_latex.bat
@@ -5,3 +5,6 @@ rm "utkast till utkast till rapport.aux"
 rm "utkast till utkast till rapport.pdf"
 
 pdflatex "utkast till utkast till rapport.tex"
+bibtex "utkast till utkast till rapport"
+pdflatex "utkast till utkast till rapport.tex"
+pdflatex "utkast till utkast till rapport.tex"
