884cf7418eb4277bffb9a02c876bb42cd7df9dce
Svante Nilsson <svante.n@live.se>
2015-04-27 22:28:18 +0200

    compile_latex.bat now removes files before compile

--- Project/Report/compile_latex.bat
+++ Project/Report/compile_latex.bat
@@ -1,8 +1,10 @@
 @ECHO off
 
-rm "utkast till utkast till rapport.log"
-rm "utkast till utkast till rapport.aux"
 rm "utkast till utkast till rapport.pdf"
+rm "utkast till utkast till rapport.aux"
+rm "utkast till utkast till rapport.blg"
+rm "utkast till utkast till rapport.bbl"
+rm "utkast till utkast till rapport.log"
 
 pdflatex "utkast till utkast till rapport.tex"
 bibtex "utkast till utkast till rapport"
