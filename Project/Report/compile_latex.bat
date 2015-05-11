effd50d0f9600a1bb2418c2ad4203e2a92692461
Svante Nilsson <svante.n@live.se>
2015-05-11 19:36:46 +0200

    Moved some background to theory, renamed .tex

--- Project/Report/compile_latex.bat
+++ Project/Report/compile_latex.bat
@@ -1,12 +1,13 @@
 @ECHO off
 
-rm "utkast till utkast till rapport.pdf"
-rm "utkast till utkast till rapport.aux"
-rm "utkast till utkast till rapport.blg"
-rm "utkast till utkast till rapport.bbl"
-rm "utkast till utkast till rapport.log"
+rm "report_draft.pdf"
+rm "report_draft.aux"
+rm "report_draft.blg"
+rm "report_draft.bbl"
+rm "report_draft.log"
+rm "report_draft.toc"
 
-pdflatex "utkast till utkast till rapport.tex"
-bibtex "utkast till utkast till rapport"
-pdflatex "utkast till utkast till rapport.tex"
-pdflatex "utkast till utkast till rapport.tex"
+pdflatex "report_draft.tex"
+bibtex "report_draft"
+pdflatex "report_draft.tex"
+pdflatex "report_draft.tex"
